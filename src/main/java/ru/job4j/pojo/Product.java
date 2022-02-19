71a7af5d30248cf4a0880acbe6994320f02808ed
Vetoshkin-Oleg <voa2014@google.com>
2022-02-20 00:53:25 +0500

    4. Сравнение моделей. Метод equals [#174893]

--- src/main/java/ru/job4j/pojo/Product.java
+++ src/main/java/ru/job4j/pojo/Product.java
@@ -1,5 +1,7 @@
 package ru.job4j.pojo;
 
+import java.util.Objects;
+
 public class Product {
     private String name;
     private int count;
@@ -23,5 +25,18 @@ public class Product {
 
     public void setCount(int count) {
         this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return getCount() == product.getCount() && Objects.equals(getName(), product.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getCount());
     }
 }
