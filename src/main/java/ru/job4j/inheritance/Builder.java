f21d4663b1fd6347788676a57919d35f173f98e5
Vetoshkin-Oleg <voa2014@google.com>
2022-02-19 22:41:25 +0500

    3. Реализация профессий в коде [#6837 #266470]

--- src/main/java/ru/job4j/inheritance/Builder.java
+++ src/main/java/ru/job4j/inheritance/Builder.java
@@ -3,10 +3,6 @@ package ru.job4j.inheritance;
 public class Builder extends Engineer {
     private boolean fearOfHeights = false;
 
-    public Builder(String name, String surname, String education, String birthday) {
-        super(name, surname, education, birthday);
-    }
-
     public Builder(String name, String surname, String education, String birthday, boolean fearOfHeights) {
         super(name, surname, education, birthday);
         this.fearOfHeights = fearOfHeights;
--- src/main/java/ru/job4j/inheritance/Surgeon.java
+++ src/main/java/ru/job4j/inheritance/Surgeon.java
@@ -3,10 +3,6 @@ package ru.job4j.inheritance;
 public class Surgeon extends Doctor {
     private String specialization = "Я - кардиохирург";
 
-    public Surgeon(String name, String surname, String education, String birthday) {
-        super(name, surname, education, birthday);
-    }
-
     public Surgeon(String name, String surname, String education, String birthday, String specialization) {
         super(name, surname, education, birthday);
         this.specialization = specialization;
