30086c47aacf10dc1b42dafbf96898636be80280
Vetoshkin-Oleg <voa2014@google.com>
2022-06-20 21:37:53 +0500

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.Assert.assertFalse;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void addDublicate() {
+        Citizen citizenOne = new Citizen("2f44a", "Petr Arsentev1");
+        Citizen citizenTwo = new Citizen("2f44a", "Petr Arsentev2");
+        PassportOffice office = new PassportOffice();
+        office.add(citizenOne);
+        office.add(citizenTwo);
+//        assertFalse(office.get(citizenTwo.getPassport()).getUsername(), is("Petr Arsentev2"));
+    }
+}
