d34fd2e30aa739425a4d466ed9bf0c779f9c62d0
Vetoshkin-Oleg <voa2014@google.com>
2022-08-11 22:31:36 +0500

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -12,7 +12,6 @@ public class JobSorter {
         );
         System.out.println(jobs);
         jobs.sort(new SortByNameJob());
-//        jobs.sort(new SortDescByNameJob());
         System.out.println(jobs);
     }
 }
