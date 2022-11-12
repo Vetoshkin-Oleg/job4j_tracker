8b49c70dd1a0c6624f46e0ee712bd969b97899d5
Vetoshkin-Oleg <voa2014@google.com>
2022-11-12 11:44:05 +0500

    3. Уникальность элементов и сортировка. [#110225]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -23,4 +23,25 @@ public class ProfilesTest {
         List<Address> expected = Arrays.asList(first, second, third);
         assertThat(result).containsAll(expected);
     }
+
+    @Test
+    public void whenTestMethodCollectSortWithoutDuplicate() {
+        Address third = new Address("City3", "Street3", 3, 3);
+        Address first = new Address("City1", "Street1", 1, 1);
+        Address second = new Address("City2", "Street2", 2, 2);
+        Address fourth = new Address("City1", "Street1", 1, 1);
+        Address seventh = new Address("City3", "Street3", 3, 3);
+        Address fifth = new Address("City2", "Street2", 2, 2);
+        List<Profile> profiles = Arrays.asList(
+                new Profile(second),
+                new Profile(fourth),
+                new Profile(fifth),
+                new Profile(first),
+                new Profile(seventh),
+                new Profile(third)
+        );
+        List<Address> result = Profiles.collectSortWithoutDuplicate(profiles);
+        List<Address> expected = Arrays.asList(first, second, third);
+        assertThat(result).isEqualTo(expected);
+    }
 }
