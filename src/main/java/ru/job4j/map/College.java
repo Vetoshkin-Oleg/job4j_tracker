ca062052a38aad8adc4196b090c7d3aa6d238161
Vetoshkin-Oleg <voa2014@google.com>
2022-11-18 20:56:26 +0500

    3. Optional - empty, of [#214711]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -1,6 +1,7 @@
 package ru.job4j.map;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class College {
@@ -10,38 +11,25 @@ public class College {
         this.students = students;
     }
 
-    public Student findByAccount(String account) {
-        return students.keySet()
+    public Optional<Student> findByAccount(String account) {
+        Optional<Student> rsl;
+        rsl = students.keySet()
                 .stream()
                 .filter(s -> s.getAccount().equals(account))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
+        return rsl;
     }
 
-    public Subject2 findBySubjectName(String account, String name) {
-        Student a = findByAccount(account);
-        if (a != null) {
+    public Optional<Subject2> findBySubjectName(String account, String name) {
+        Optional<Student> a = findByAccount(account);
+        Optional<Subject2> rsl = Optional.empty();
+        if (a.isPresent()) {
             return students.get(a)
                     .stream()
                     .filter(s -> s.getName().equals(name))
-                    .findFirst()
-                    .orElse(null);
+                    .findFirst();
         }
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Map<Student, Set<Subject2>> students = Map.of(new Student("Student", "000001", "201-18-15"),
-                Set.of(
-                        new Subject2("Math", 70),
-                        new Subject2("English", 85)
-                )
-        );
-        College college = new College(students);
-        Student student = college.findByAccount("000001");
-        System.out.println("Найденный студент: " + student);
-        Subject2 english = college.findBySubjectName("000001", "English");
-        System.out.println("Оценка по найденному предмету: " + english.getScore());
+        return rsl;
     }
 
 }
