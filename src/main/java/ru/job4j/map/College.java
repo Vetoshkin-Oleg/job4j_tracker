c4bb43561f6af810e51a852f6eca10c09d091348
Vetoshkin-Oleg <voa2014@google.com>
2022-11-19 23:41:24 +0500

    3. Optional - empty, of [#214711 #362047]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -1,6 +1,5 @@
 package ru.job4j.map;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -14,28 +13,26 @@ public class College {
 
     public Optional<Student> findByAccount(String account) {
         Optional<Student> rsl = Optional.empty();
-        List<Student> studentList;
-        studentList = students.keySet()
-                .stream()
-                .filter(s -> s.getAccount().equals(account)).toList();
-        if (studentList.size() != 0) {
-            rsl = Optional.of(studentList.get(0));
+        for (Student s: students.keySet()) {
+            if (account.equals(s.getAccount())) {
+                rsl = Optional.of(s);
+                break;
+            }
         }
         return rsl;
     }
 
     public Optional<Subject2> findBySubjectName(String account, String name) {
-        Optional<Student> a = findByAccount(account);
         Optional<Subject2> rsl = Optional.empty();
-        List<Subject2> subject2List = null;
-        if (a.isPresent()) {
-            subject2List = students.get(a)
-                    .stream()
-                    .filter(s -> s.getName().equals(name)).toList();
-        }
-        assert subject2List != null;
-        if (subject2List.size() != 0) {
-            rsl = Optional.of(subject2List.get(0));
+        Optional<Student> s = findByAccount(account);
+        if (s.isPresent()) {
+            Set<Subject2> subjects = students.get(s.get());
+            for (Subject2 subj : subjects) {
+                if (name.equals(subj.getName())) {
+                    rsl = Optional.of(subj);
+                    break;
+                }
+            }
         }
         return rsl;
     }
