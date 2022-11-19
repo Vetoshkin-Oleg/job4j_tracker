441b313b85f98991c401a287d4075c4ab974e87d
Vetoshkin-Oleg <voa2014@google.com>
2022-11-19 10:34:07 +0500

    3. Optional - empty, of [#214711 #362047]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -1,5 +1,6 @@
 package ru.job4j.map;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -12,22 +13,29 @@ public class College {
     }
 
     public Optional<Student> findByAccount(String account) {
-        Optional<Student> rsl;
-        rsl = students.keySet()
+        Optional<Student> rsl = Optional.empty();
+        List<Student> studentList;
+        studentList = students.keySet()
                 .stream()
-                .filter(s -> s.getAccount().equals(account))
-                .findFirst();
+                .filter(s -> s.getAccount().equals(account)).toList();
+        if (studentList.size() != 0) {
+            rsl = Optional.of(studentList.get(0));
+        }
         return rsl;
     }
 
     public Optional<Subject2> findBySubjectName(String account, String name) {
         Optional<Student> a = findByAccount(account);
         Optional<Subject2> rsl = Optional.empty();
+        List<Subject2> subject2List = null;
         if (a.isPresent()) {
-            return students.get(a)
+            subject2List = students.get(a)
                     .stream()
-                    .filter(s -> s.getName().equals(name))
-                    .findFirst();
+                    .filter(s -> s.getName().equals(name)).toList();
+        }
+        assert subject2List != null;
+        if (subject2List.size() != 0) {
+            rsl = Optional.of(subject2List.get(0));
         }
         return rsl;
     }
