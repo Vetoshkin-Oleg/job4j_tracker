1a8e744a4d168ee5227774dd4a175c5c55184eaa
Vetoshkin-Oleg <voa2014@gmail.com>
2023-06-25 20:34:39 +0500

    4. Аттестация [#504878  [#504878 #329402]]

--- src/main/java/ru/job4j/hmap/Label.java
+++ src/main/java/ru/job4j/hmap/Label.java
@@ -1,4 +1,4 @@
-package ru.job4j.map;
+package ru.job4j.hmap;
 
 public record Label(String name, double score) implements Comparable<Label> {
     @Override
--- src/test/java/ru/job4j/hmap/AnalyzeByMapTest.java
+++ src/test/java/ru/job4j/hmap/AnalyzeByMapTest.java
@@ -1,6 +1,7 @@
-package ru.job4j.map;
+package ru.job4j.hmap;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.offset;
