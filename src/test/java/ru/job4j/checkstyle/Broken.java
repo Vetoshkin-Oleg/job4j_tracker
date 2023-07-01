18501304f9532d796ae276367a3957751c4e1747
Vetoshkin-Oleg <voa2014@gmail.com>
2023-07-01 09:01:09 +0500

    6. Правила Checkstyle [#504981  [#504981]]

---
+++ src/test/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,28 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEWVALUE = "";
+    private final int sizeOfEmpty = 10;
+
+    public String surname;
+
+    Broken() {
+
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+    String name;
+
+}
